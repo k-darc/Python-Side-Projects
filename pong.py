207730dd3a9cc9c95e156894111f67e2ca7a7c8b
Kevin <neoglitch822@yahoo.com>
2023-11-02 19:47:47 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -14,5 +14,20 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 while True:
     wn.update()
