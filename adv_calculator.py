f973bb1ab901977c6318612cfc506f7878d5ca26
Kevin <neoglitch822@yahoo.com>
2023-11-02 12:05:58 -0400

    Update adv_calculator.py

--- adv_calculator.py
+++ adv_calculator.py
@@ -33,5 +33,14 @@ text_result.grid(columnspan=5)
 
 btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
 btn_1.grid(row=2, column=1)
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial", 14))
+btn_2.grid(row=2, column=2)
+
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14))
+btn_plus.grid(row=2, column=4)
+
+btn_equals = tk.Button(root, text="=", command=evaluate_calculation, width=11, font=("Arial", 14))
+btn_equals.grid(row=6, column=3, columnspan=2)
+
 
 root.mainloop()
