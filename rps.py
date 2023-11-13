8081e54a1335d856da728b80029f791685729d4e
Kevin <neoglitch822@yahoo.com>
2023-11-13 16:25:02 -0500

    Rock, Paper, Scissors

--- rps.py
+++ rps.py
@@ -8,18 +8,19 @@ while playing:
     player = None
     computer = random.choice(options)
 
-    player = input("Enter a choice (rock, paper or scissors): ")
+    while player not in options:
+        player = input("Enter a choice (rock, paper or scissors): ")
 
     print(f"Player: {player}")
     print(f"Computer: {computer}")
 
     if player == computer:
-        print("It's a tie no one has won.")
+        print(f"It's a tie no one has won.", player, "does not beat", computer)
     elif player == "rock" and computer == "scissors":
-        print("You win!")
+        print("You win!", player, "beats", computer)
     elif player == "paper" and computer == "rock":
-        print(f"You win!")
+        print(f"You win!", player, "beats", computer)
     elif player == "scissors" and computer == "paper":
-        print(f"You win!")
+        print(f"You win!", player, "beats", computer)
     else:
-        print("You have lost")
+        print("You have lost", computer, "beats", player)
