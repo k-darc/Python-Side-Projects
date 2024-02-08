a504336fa96f07c39dd7a5b2f86329f53020963d
Kevin <neoglitch822@yahoo.com>
2024-02-08 16:37:48 -0500

    Update project1_diceroll.py

--- project1_diceroll.py
+++ project1_diceroll.py
@@ -11,4 +11,12 @@ def roll():
 while True:
     players = input("Enter the number of players. (1 - 4): ")
     if players.isdigit():
-        
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("Invalid, try again.")
+
+print(players)
