323ea251e4650ec650a141438f79505e784bf142
Kevin <neoglitch822@yahoo.com>
2024-02-09 11:41:30 -0500

    Update project2_madlibs.py

--- project2_madlibs.py
+++ project2_madlibs.py
@@ -22,4 +22,8 @@ for word in words:
     answer = input("Enter a word for " + word + ": ")
     answers[word] = answer
 
-print(answers)
+for word in words:
+    story = story.replace(word, answers[word])
+
+print()
+print(story)
