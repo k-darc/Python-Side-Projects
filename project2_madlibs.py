ae5e8fe6884a0b7557f7402fa11739eb68fa9de3
Kevin <neoglitch822@yahoo.com>
2024-02-09 11:30:58 -0500

    Update project2_madlibs.py

--- project2_madlibs.py
+++ project2_madlibs.py
@@ -7,9 +7,13 @@ start_of_word = -1
 target_start = "<"
 target_end = ">"
 
-for i, chat in enumerate(story):
-    if chat == target_start:
+for i, char in enumerate(story):
+    if char == target_start:
         start_of_word = i
 
-    if charr == target_end and start_of_word != -1:
+    if char == target_end and start_of_word != -1:
         word = story[start_of_word: i + 1]
+        words.append(word)
+        start_of_word = -1
+
+print(words)
