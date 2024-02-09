a94a304095a2c9a230ed511e5ca29d2d44c40925
Kevin <neoglitch822@yahoo.com>
2024-02-09 11:29:18 -0500

    Update project2_madlibs.py

--- project2_madlibs.py
+++ project2_madlibs.py
@@ -4,5 +4,12 @@ with open("story.txt", "r") as f:
 words = []
 start_of_word = -1
 
+target_start = "<"
+target_end = ">"
+
 for i, chat in enumerate(story):
-    
+    if chat == target_start:
+        start_of_word = i
+
+    if charr == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
