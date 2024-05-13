d7217c544cea96082672d18c9b48d1844222b218
Kevin <neoglitch822@yahoo.com>
2024-05-13 14:18:40 -0400

    Update rps_beau.py

--- rps_beau.py
+++ rps_beau.py
@@ -1,7 +1,7 @@
 import random
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
     options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
