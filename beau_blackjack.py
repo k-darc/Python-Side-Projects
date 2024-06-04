0c2178205f69091b7353a5a22429654edac46ed0
Kevin <neoglitch822@yahoo.com>
2024-06-04 15:45:26 -0400

    Update beau_blackjack.py

--- beau_blackjack.py
+++ beau_blackjack.py
@@ -1,5 +1,5 @@
-suit = "hearts"
-rank = "k"
-value = 10
-
-print(f "Your card is: {rank}")
+cards = []
+suits = ["spades", "clubs", "hearts", "diamonds"]
+ranks = ["A", 2:10, "J", "Q", "K"]
+for suit in suits:
+    print([suit, ranks[0]])
