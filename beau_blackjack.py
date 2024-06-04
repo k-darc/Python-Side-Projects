924c17b9e72bde9ee231a4198a3f0a7cafdc2797
Kevin <neoglitch822@yahoo.com>
2024-06-04 15:47:09 -0400

    Update beau_blackjack.py

--- beau_blackjack.py
+++ beau_blackjack.py
@@ -1,8 +1,7 @@
 import random
-
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+ranks = ["Ace": 11, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
@@ -18,18 +17,4 @@ def deal(number):
     return cards_dealt
 
 shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-
-if rank == "Ace":
-    value = 11
-elif rank == "J"or rank == "Queen" or rank == "King":
-    value = 10
-else:
-    value = rank
-
-rank_dict = {"rank": rank, "value": value}
-
-
-print(rank_dict["rank"], rank_dict["value"])
+card = deal(1)[0]
