f6402bc622c29ddcc423e1603ffe3a5ada2dcbcc
Kevin <neoglitch822@yahoo.com>
2024-06-04 15:45:53 -0400

    Update beau_blackjack.py

--- beau_blackjack.py
+++ beau_blackjack.py
@@ -1,5 +1,8 @@
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", 2:10, "J", "Q", "K"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
-    print([suit, ranks[0]])
+    for rank in ranks:
+        cards.append([suit, rank])
+
+print(cards)
