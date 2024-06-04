93d59fa134fad070af3ee758f1ca1ecefe7c2eb3
Kevin <neoglitch822@yahoo.com>
2024-06-04 16:10:12 -0400

    Update beau_blackjack.py

--- beau_blackjack.py
+++ beau_blackjack.py
@@ -5,7 +5,7 @@ class Card:
         self.suit = suit
         self.rank = rank
     def __str__(self):
-        return self.rank["rank"] + " of " + self.suit
+        return f"{self.rank['rank']} of {self.suit}"
 
 class Deck:
     def __init__(self):
@@ -28,7 +28,7 @@ class Deck:
             ]
         for suit in suits:
             for rank in ranks:
-                self.cards.append([suit, rank])
+                self.cards.append(Card(suit, rank))
 
     def shuffle(self):
         if len(self.cards) > 1:
@@ -42,5 +42,34 @@ class Deck:
                 cards_dealt.append(card)
         return cards_dealt
 
-card1 = Card("hearts", {"rank": "J", "value": 10})
-print(card1)
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "Ace":
+                has_ace = True
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value():
+        return self.value
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+print(hand.cards[0], hand.cards[1])
