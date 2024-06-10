551041f527430e1b77421a15fcac72b0247873c4
Kevin <neoglitch822@yahoo.com>
2024-06-10 17:03:39 -0400

    Update guessing.py

--- guessing.py
+++ guessing.py
@@ -6,3 +6,16 @@ print("Welcome to the Numbers Game")
 print("I've picked a number between 1 and 100. You have 3 guesses.")
 print("I'll give you some hints each time you guess... good luck!")
 
+number_of_guesses = 0
+
+while True:
+    guess = int(input("Pick a number: "))
+    number_of_guesses += 1
+
+    if guess < number:
+        print("Too low. Try again")
+    elif guess > number:
+        print("Too high. Try again.")
+    else:
+        print(f"Congratulations! You guesses {number} in {number_of_guesses}!")
+        break
