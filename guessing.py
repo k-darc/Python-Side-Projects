b33d3553f2229ad9fd1a1a9f62870f74aff2c2be
Kevin <neoglitch822@yahoo.com>
2024-06-10 16:49:29 -0400

    Create guessing.py
    first draft, number guessing game

---
+++ guessing.py
@@ -0,0 +1,8 @@
+import random
+
+number = random.randint(1, 100)
+
+print("Welcome to the Numbers Game")
+print("I've picked a number between 1 and 100. You have 3 guesses.")
+print("I'll give you some hints each time you guess... good luck!")
+
