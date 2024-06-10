4cbfc0e1646fadb7d238ce51edfd6284d9936b88
Kevin <neoglitch822@yahoo.com>
2024-06-10 17:33:22 -0400

    Update guessing.py
    final. finished, may limit guesses in future

--- guessing.py
+++ guessing.py
@@ -1,33 +1,34 @@
 import random
 
-number = random.randint(1, 100)
+def main():
+    number = random.randint(1, 100)
 
-print("Welcome to the Numbers Game")
-print("I've picked a number between 1 and 100. You have 3 guesses.")
-print("I'll give you some hints each time you guess... good luck!")
+    print("Welcome to the Numbers Game")
+    print("I've picked a number between 1 and 100.")
+    print("I'll give you some hints each time you guess... good luck!")
 
-number_of_guesses = 0
+    guesses = 0
 
-while True:
-    guess = int(input("Pick a number: "))
-    number_of_guesses += 1
+    while True:
+        guess = int(input("Pick a number: "))
+        guesses += 1
 
-    if guess < number:
-        print("Too low. Try again")
-    elif guess > number:
-        print("Too high. Try again.")
+        if guess < number:
+            print("Too low. Try again")
+        elif guess > number:
+            print("Too high. Try again.")
+        else:
+            print(f"Congratulations! You guesses {number} in {guesses}!")
+            break
+
+    # Restart
+    play_again = input("Do you want to play again (Y/N): ").lower()
+
+    if play_again != "y":
+        print("Thank you for playing!")
     else:
-        print(f"Congratulations! You guesses {number} in {number_of_guesses}!")
-        break
-
-# Restart
-
-restart = input("Do you want to play again (Y/N): ")
-
-if restart != "y":
-    print("Thank you for playing!")
-    break
-else:
-    number = random.randint(1,100)
-    number_of_guesses = 0
-    print("I've picked a new number! Let's play again!")
+        number = random.randint(1, 100)
+        guesses = 0
+        print("I've picked a new number! Let's play again!")
+        main()
+main()
