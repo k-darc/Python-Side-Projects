b7c823f141ff6598ee73fa6823b8022e2047150d
Kevin <neoglitch822@yahoo.com>
2024-06-11 13:11:38 -0400

    Update calculate_volume.py

--- calculate_volume.py
+++ calculate_volume.py
@@ -1,6 +1,6 @@
 import math
 
-def calculate(radius):
+def calculate_area(radius):
     return math.pi * radius ** 2
 
 def calculate_volume(radius, height):
@@ -13,7 +13,7 @@ def main():
         height = float(input("Enter the Height of the cylinder: "))
 
         if radius <= 0 or height <= 0:
-            raise Value Error("Radius and Height must be valid numbers")
+            raise ValueError("Radius and Height must be valid numbers")
 
         area = calculate_area(radius)
         volume = calculate_volume(radius, height)
@@ -21,3 +21,10 @@ def main():
         print("The area of the cylinder is: {area:.2f}")
         print(f"The volume of the cylinder is: {volume:.2f}")
 
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    main()
