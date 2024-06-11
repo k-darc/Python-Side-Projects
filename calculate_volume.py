dfd199d087041f52f2413b33e927db3a75becfbf
Kevin <neoglitch822@yahoo.com>
2024-06-11 12:58:13 -0400

    Update calculate_volume.py

--- calculate_volume.py
+++ calculate_volume.py
@@ -1,1 +1,15 @@
 import math
+
+def calculate(rad):
+    return math.pi * rad ** 2
+
+def calculate_volume(rad,hgt):
+    base = calculate_area(rad)
+    return base * hgt
+
+def main():
+    rad = float(input("Enter the Radius: "))
+    hgt = float(input("Enter the Height: "))
+
+
+
