9ebeb2bea6024001be479480e97aeefcd1595576
Kevin <neoglitch822@yahoo.com>
2024-06-12 12:22:54 -0400

    Update calculator_mortgage.py

--- calculator_mortgage.py
+++ calculator_mortgage.py
@@ -1,4 +1,12 @@
-def calculate_months(mortgage_value, interest, term):
-    monthly_interest_rate = interest / 100 / 12
-    term_months = term * 12
-    monthly_payment = (mortgage_value * monthly_interest_rate) / (1 - (1 + monthly_interest) ** -term_months)
+def main():
+
+    def calculate_months_to_pay_off(mortgage_value, interest_rate, term_years):
+        monthly_interest_rate = interest_rate / 100 / 12
+        term_months = term_years * 12
+        monthly_payment = (mortgage_value * monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** -term_months)
+        months_to_payoff = term_months
+        return months_to_payoff
+
+    mortgage_value = input("Enter remaining value of your mortgage: $")
+    interest_rate = input("Enter your interest rate as a percentage: ")
+    term_years = input("Enter the term of your mortgage: ")
