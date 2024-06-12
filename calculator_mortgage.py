e0772bbfd9823d032b0b34ce66a0ab1484ea318c
Kevin <neoglitch822@yahoo.com>
2024-06-12 12:28:59 -0400

    Update calculator_mortgage.py

--- calculator_mortgage.py
+++ calculator_mortgage.py
@@ -1,12 +1,16 @@
+
+def calculate_months_to_payoff(mortgage_value, interest_rate, term_years):
+    monthly_interest_rate = interest_rate / 100 / 12
+    term_months = term_years * 12
+    monthly_payment = (mortgage_value * monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** -term_months)
+    months_to_payoff = term_months
+    return months_to_payoff
+
 def main():
-
-    def calculate_months_to_pay_off(mortgage_value, interest_rate, term_years):
-        monthly_interest_rate = interest_rate / 100 / 12
-        term_months = term_years * 12
-        monthly_payment = (mortgage_value * monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** -term_months)
-        months_to_payoff = term_months
-        return months_to_payoff
-
     mortgage_value = input("Enter remaining value of your mortgage: $")
     interest_rate = input("Enter your interest rate as a percentage: ")
     term_years = input("Enter the term of your mortgage: ")
+    months_to_payoff = calculate_months_to_payoff(mortgage_value, interest_rate, term_years)
+    print("It will take approzimately {} months to payoff your mortgage.".format(months_to_payoff))
+
+main()
