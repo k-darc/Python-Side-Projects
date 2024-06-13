1fe654beeab9a361cf992a7309518509bedf4451
Kevin <neoglitch822@yahoo.com>
2024-06-13 15:29:05 -0400

    Update game_harrypotter.py

--- game_harrypotter.py
+++ game_harrypotter.py
@@ -1,5 +1,10 @@
 import random
 
-def questions():
-    questions = random.sample(list(questions.keys*()), 3)
-    return {question: questions} for question in questions
+def selected_questions():
+    selected_questions = random.sample(list(questions.keys*()), 3)
+    return {question: questions} for question in selected_questions
+
+def quizgame():
+    selected_questions = questions()
+    correct = 0
+    total_questions = len(questions)
