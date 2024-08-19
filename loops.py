057709025a05c084d18693032cae2f9fdc49bc1d
Kevin <neoglitch822@yahoo.com>
2024-08-19 12:53:08 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,6 @@
+students = [
+    "Harry Potter", "Hermione Granger", "Ronald Weasley", "Padma Patel", "Cedric Diggory", "Draco Malfoy"
+]
+for student in students: # This will print everyone
+    print(student[0:-1]) #print(student[0] will print the first letter for each name and print(student[0:-1]) will print the whole name but not the last letter)
+
